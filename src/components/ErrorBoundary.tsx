28097d522f10d0cf90977b7b163ef9347ee2b227
Desert <2864963532@qq.com>
2024-01-24 20:31:37 +0800

    feat: assets

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,19 @@
+import { useRouteError } from 'react-router-dom'
+
+function ErrorBoundary() {
+  const err = useRouteError() as any
+  return (
+    <div>
+      <p>
+        出错啦～
+      </p>
+      <p>
+        错误信息:
+        {' '}
+        {err.message}
+      </p>
+    </div>
+  )
+}
+
+export default ErrorBoundary
