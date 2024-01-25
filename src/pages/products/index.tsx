9d5d32eb64e3a07135d76e04dba346379f9980e9
Desert <2864963532@qq.com>
2024-01-25 13:34:06 +0800

    fix: 产品样式

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -10,23 +10,25 @@ function Products() {
       <div className={style.topShow}>
         <h2>工业3d模型解析平台</h2>
         <span>网页上直接浏览3d模型，尺寸测量、3D标注、特征识别、制造成本分析</span>
-        <div className={style.button}>
+        <div className={style.interactButton}>
           <button>申请试用</button>
           <button>联系报价</button>
         </div>
         <div className={style.video}>
           <video src=""></video>
         </div>
-        <p>
-          搭建高性能
-          <span>web端3D可视化平台</span>
-          ，提供更快、更准确的CAD
-        </p>
-        <p>
-          数据解析
-          <span>API</span>
-          接口，为构建工业3D应用程序提供优选解决方案
-        </p>
+        <div className={style.videoDetail}>
+          <p>
+            搭建高性能
+            <span>web端3D可视化平台</span>
+            ，提供更快、更准确的CAD
+          </p>
+          <p>
+            数据解析
+            <span>API</span>
+            接口，为构建工业3D应用程序提供优选解决方案
+          </p>
+        </div>
         <div className={style.topLine}>
           <p>在线预览多种格式文件效果</p>
           <a href="">tuo visual</a>
@@ -46,32 +48,53 @@ function Products() {
         <p>工业3D模型解析平台提供了API接口，实现客户快速的在线浏览3d模型、测量、3D标注、特征识别、制造成本分析等功能</p>
         <FeaturesCard />
       </div>
+      <hr />
       <div className={style.parse}>
-        <div className={style.parseContent}>
-          <h3>工业3d模型解析平台</h3>
-          <p>
-            通过对工业3d模型进行解析，转化成
-            <a>tuo-visual</a>
-            格式数据和模型整体缩略图。
+        <h1>工业3d模型解析平台</h1>
+        <p>
+          通过对工业3d模型进行解析，转化成
+          <a>tuo-visual</a>
+          格式数据和模型整体缩略图。
 
-          </p>
-          <p>
-            企业可以调用平台提供的
-            <span>API接口</span>
-            ，实现在客户端网页上直接浏览3d模型，提供
-          </p>
-          <p>
-            模型尺寸测量、3D标注、特征识别、制造成本分析等功能
-          </p>
-        </div>
+        </p>
+        <p>
+          企业可以调用平台提供的
+          <span>API接口</span>
+          ，实现在客户端网页上直接浏览3d模型，提供
+        </p>
+        <p>
+          模型尺寸测量、3D标注、特征识别、制造成本分析等功能
+        </p>
       </div>
       <div className={style.palyFree}>
-        <div className={style.interact}>
-          <h3>我是企业</h3>
-
-        </div>
-        <div className={style.PaidForms}>
-
+        <div className={style.paidContent}>
+          <div className={style.interact}>
+            <div className={style.crowd}>
+              <h3>我是企业</h3>
+              <p>
+                免费试用10次，按需选配
+                按次计费，买多省多
+              </p>
+              <div>
+                <button>申请试用</button>
+                <button>申请购买</button>
+              </div>
+            </div>
+            <div className={style.crowd}>
+              <h3>我是个人</h3>
+              <p>
+                免费试用10次，按需选配
+                按次计费，买多省多
+              </p>
+              <div>
+                <button>申请试用</button>
+                <button>申请购买</button>
+              </div>
+            </div>
+          </div>
+          <div className={style.PaidForms}>
+            1212
+          </div>
         </div>
       </div>
       <div className={style.TrialForms}>
