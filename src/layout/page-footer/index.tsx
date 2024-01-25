189be81391aab66a72d8c389cbbd7c22cf6abb18
iridescent <2389504513@qq.com>
2024-01-25 17:53:52 +0800

    ✨ feat: 完善Footer

--- src/layout/page-footer/index.tsx
+++ src/layout/page-footer/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import styles from './index.module.scss'
+import RelationsImg from '@/assets/imgs/tiangong-relations.png'
+import SymbolImg from '@/assets/imgs/tiangong-symbol.png'
 
 function PageFooter() {
   return (
     <footer>
       <div className={styles.header}></div>
       <div className={styles.body}>
-        <div className={styles.bodyLeft}></div>
-        <div className={styles.bodyCenter}></div>
-        <div className={styles.bodyRight}></div>
+        <div className={styles.bodyLeft}>
+          <div className={styles.bodyLeftTitle}>联系我们</div>
+          <div className={styles.bodyLeftDesc}>18574652970</div>
+        </div>
+        <div className={styles.bodyCenter}>
+          <div className={styles.bodyCenterBox}>邮箱: tiangongtuxue123@sina.com</div>
+          <div className={styles.bodyCenterBox}>地址: xxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
+        </div>
+        <div className={styles.bodyRight}>
+          <img src={RelationsImg} className={styles.bodyRightImg} />
+        </div>
       </div>
       <div className={styles.footer}>
         <div>厦门点射科技有限公司</div>
-        <img src=""></img>
+        <img src={SymbolImg} className={styles.footerImg} />
         <div>Copyright 2020-2023</div>
       </div>
     </footer>
