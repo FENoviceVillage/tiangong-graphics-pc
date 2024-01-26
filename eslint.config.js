a8ff59b42b6e80b55486f6cecba2cfb9285cccd8
iridescent <2389504513@qq.com>
2024-01-26 11:04:55 +0800

    ✨ feat: 添加依赖，创建动态内容

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,11 @@
-// eslint.config.js
 const antfu = require('@antfu/eslint-config').default
 
 module.exports = antfu({
   react: true,
   typescript: true,
   formatters: {
-    /**
-     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
-     * By default uses Prettier
-     */
     css: true,
-    /**
-     * Format HTML files
-     * By default uses Prettier
-     */
     html: true,
-    /**
-     * Format Markdown files
-     * Supports Prettier and dprint
-     * By default uses Prettier
-     */
     markdown: 'prettier',
   },
 })
---
+++ src/pages/dynamic/components/category/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DynamicCategory() {
+  return (
+    <div>DynamicCategory</div>
+  )
+}
+
+export default DynamicCategory
