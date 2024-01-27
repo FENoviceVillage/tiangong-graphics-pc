7416ad42c91c62e417a1258916caed2c9e4f2ac8
Desert <2864963532@qq.com>
2024-01-27 20:45:42 +0800

    ✨ feat: 修改全局scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import PageFooter from '@/layout/page-footer'
 import PageHeader from '@/layout/page-header'
 import PageBody from '@/layout/page-body'
+import './assets/styles/globals.scss'
 
 function App() {
   return (
