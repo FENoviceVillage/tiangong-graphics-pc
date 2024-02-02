32e4c67e72fcfce786580feac33233b870303a8b
Desert <2864963532@qq.com>
2024-02-02 16:56:31 +0800

    fix: 修改UI

--- src/pages/products/components/abilityCard/index.tsx
+++ src/pages/products/components/abilityCard/index.tsx
@@ -1,17 +1,33 @@
+import { useEffect, useState } from 'react'
 import Pushpins from '../../../../assets/img/products/Pushpins.png'
 import style from './index.module.scss'
 
 interface Props {
   abilityTitle?: string
-  abilitySubtitle?: string
+  abilitySubtitle: string | undefined
 }
 
 function AbilityCard(props: Props) {
   const { abilityTitle, abilitySubtitle } = props
+  const regex = /(\S+)\s+(.+)/
+
+  const matches = abilityTitle?.match(regex)
+  const Title = () => {
+    if (matches)
+      return [matches[1], matches[2]]
+    else
+      return [abilityTitle]
+  }
   return (
     <div className={style.card}>
       <img src={Pushpins} alt="" className={style.cardPushpins} />
-      <h3>{abilityTitle}</h3>
+      <div className={abilitySubtitle ? '' : style.cardBottom}>
+        {
+        Title().map((item, index) =>
+          <h3 key={index}>{item}</h3>,
+        )
+      }
+      </div>
       {
         abilitySubtitle && <span>{abilitySubtitle}</span>
       }
