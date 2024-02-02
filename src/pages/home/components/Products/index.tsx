33b7671d9cba5d1eab16c03f7cd9767124395929
zhangqiao <3279313207@qq.com>
2024-02-02 14:31:18 +0800

    首页调整

--- src/pages/home/components/Products/index.tsx
+++ src/pages/home/components/Products/index.tsx
@@ -8,7 +8,7 @@ function Index() {
     <div className={styles.flex}>
       {
         products.map((product, index) => {
-          return index % 2 === 0 ? <Product key={product.id} data={product?.attributes} /> : <Product key={product.id} data={product?.attributes} isVideoleft={false} />
+          return index % 2 === 0 ? <Product key={product.id} data={product?.attributes} index={index + 1} /> : <Product key={product.id} data={product?.attributes} isVideoleft={false} index={index + 1} />
         })
       }
     </div>
